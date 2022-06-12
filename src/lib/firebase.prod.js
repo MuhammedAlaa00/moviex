ef40c4289a837b6186a1ea04c92538c59bbec6a3
MuhammedAlaa00 <36922989+mohamedalaa00@users.noreply.github.com>
2022-06-12 17:25:14 +0200

    update faqs.json file

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,15 +1,15 @@
-import Firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 const config = {
   apiKey: "AIzaSyADEBjCJuy8OF9aUQOFQmvmNLs2fwz8EtU",
   authDomain: "movie-x-1c5a6.firebaseapp.com",
   projectId: "movie-x-1c5a6",
   storageBucket: "movie-x-1c5a6.appspot.com",
   messagingSenderId: "100234215493",
-  appId: "1:100234215493:web:959cd332774ce6d654a32c"
+  appId: "1:100234215493:web:959cd332774ce6d654a32c",
 };
 const firebase = Firebase.initializeApp(config);
-export const auth = firebase.auth(firebase)
-export const provider  = new Firebase.auth.FacebookAuthProvider()
-export {firebase}
+export const auth = firebase.auth(firebase);
+export const provider = new Firebase.auth.FacebookAuthProvider();
+export { firebase };
